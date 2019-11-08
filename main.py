100514454fc5d2ad978e85199abbb742cf26bb9b
michaelsizonenko <dethline@te.net.ua>
2019-11-08 10:06:32 +0200

    dialog

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import json
 import slack
+import requests
 from flask import Flask, escape, request
 
 app = Flask(__name__)
@@ -22,6 +24,59 @@ def slack():
     return data.get("challenge")
 
 
+@app.route('/submit-ticket', methods=["GET", "POST"])
+def submit_ticket():
+    print("Submit ticket received !")
+
+
 @app.route('/command', methods=["GET", "POST"])
 def command():
     print("Command received !")
+
+    data = request.form.to_dict()
+
+    print(f"Command form : {data}")
+    text = data.get("text")
+    trigger_id = data.get("trigger_id")
+    print(f"Trigger ID : {trigger_id}")
+
+    r = requests.post(
+        "https://slack.com/api/dialog.open",
+        data=json.dumps({
+            "dialog": {
+                "callback_id": "submit-ticket",
+                "title": "Request a Ride",
+                "submit_label": "Request",
+                "state": "Limo",
+                "elements": [
+                    {
+                        "type": "text",
+                        "label": "Pickup Location",
+                        "name": "loc_origin"
+                    },
+                    {
+                        "type": "text",
+                        "label": "Dropoff Location",
+                        "name": "loc_destination"
+                    }
+                ]
+            },
+            "trigger_id": trigger_id,
+        }),
+        headers={
+            "Authorization": "Bearer {token}".format(token=slack_client_token),
+            "Content-Type": "application/json;charset=utf-8"
+        }
+    )
+    response_status = r.status_code
+    print(f"Response status : {response_status}")
+    response_json = r.json()
+    print(f"Response json : {response_json}")
+
+    return response_json
+
+
+@app.route('/interactive', methods=["GET", "POST"])
+def interactive():
+    print("Interactive received !")
+    return {'ok': True}
