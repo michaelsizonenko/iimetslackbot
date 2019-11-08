fa99af197a284164f6d2aaa283ed910436af6305
michaelsizonenko <dethline@te.net.ua>
2019-11-08 15:15:09 +0200

    thread usage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import re
 import json
+from threading import Thread
+
 import slack
 import requests
 import validators
@@ -10,10 +12,11 @@ from flask import Flask, escape, request, make_response
 app = Flask(__name__)
 
 slack_client_token = os.environ["SLACK_CLIENT_TOKEN"]
-client_web_client = slack.WebClient(token=slack_client_token)
+client = slack.WebClient(token=slack_client_token)
 
 pattern = r"Your request was submitted sucessfully. The request number is <strong>[a-zA-Z-0-9]+<\/strong>"
 re_obj = re.compile(pattern)
+
 
 @app.route('/')
 def hello():
@@ -45,53 +48,71 @@ def create_ticket():
     trigger_id = data.get("trigger_id")
     print(f"Trigger ID : {trigger_id}")
 
+    r = client.dialog_open(
+        dialog={
+            "callback_id": "submit-ticket",
+            "title": "Create a ticket",
+            "submit_label": "Request",
+            "state": "Limo",
+            "elements": [
+                {
+                    "type": "text",
+                    "label": "Full name",
+                    "name": "fullname"
+                },
+                {
+                    "type": "text",
+                    "label": "Email",
+                    "subtype": "email",
+                    "name": "email"
+                },
+                {
+                    "type": "text",
+                    "label": "Subject",
+                    "name": "subject",
+                    "value": text
+                },
+                {
+                    "type": "textarea",
+                    "label": "Content",
+                    "name": "content"
+                }
+            ]
+        },
+        trigger_id=trigger_id
+    )
+    if r.get("ok"):
+        return "Dialog created"
+    return "Error occurred"
+
+
+def send_create_ticket_request(fullname, email, subject, content):
     r = requests.post(
-        "https://slack.com/api/dialog.open",
-        data=json.dumps({
-            "dialog": {
-                "callback_id": "submit-ticket",
-                "title": "Create a ticket",
-                "submit_label": "Request",
-                "state": "Limo",
-                "elements": [
-                    {
-                        "type": "text",
-                        "label": "Full name",
-                        "name": "fullname"
-                    },
-                    {
-                        "type": "text",
-                        "label": "Email",
-                        "subtype": "email",
-                        "name": "email"
-                    },
-                    {
-                        "type": "text",
-                        "label": "Subject",
-                        "name": "subject",
-                        "value": text
-                    },
-                    {
-                        "type": "textarea",
-                        "label": "Content",
-                        "name": "content"
-                    }
-                ]
-            },
-            "trigger_id": trigger_id,
-        }),
+        "http://iimet.wwwshine.supersitedns.com/project/API/examples/ticket_form.php",
+        data={
+            "page": "general",
+            "department_id": 1,
+            "creator_full_name": fullname,
+            "creator_email": email,
+            "type_id": 1,
+            "priority_id": 1,
+            "subject": subject,
+            "contents": content,
+            "submit_general": "Submit"
+        },
         headers={
-            "Authorization": "Bearer {token}".format(token=slack_client_token),
-            "Content-Type": "application/json;charset=utf-8"
+            "Content-Type": "application/x-www-form-urlencoded"
         }
     )
-    response_status = r.status_code
-    print(f"Response status : {response_status}")
-    response_json = r.json()
-    print(f"Response json : {response_json}")
-    if not response_json.get("ok"):
-        return "Error occurred"
-    return "Dialog created"
+    print(f"Created form response code {r.status_code}")
+    text_response = r.text
+    # print(f"Created form response text : {text_response}")
+    response_message = re_obj.search(text_response)
+    if not response_message:
+        return "Could not create a ticket !"
+    tree = html.fromstring(response_message.group())
+    ticket_id = tree.xpath("strong")[0].text_content()
+    print(f"Ticket ID : {ticket_id}")
 
 
 @app.route('/interactive', methods=["GET", "POST"])
@@ -108,31 +129,7 @@ def interactive():
         return "something goes wrong"
     if not validators.email(submission.get("email")):
         return "Invalid e-mail ! Please correct your data !"
-    r = requests.post(
-        "http://iimet.wwwshine.supersitedns.com/project/API/examples/ticket_form.php",
-        data={
-            "page": "general",
-            "department_id": 1,
-            "creator_full_name": submission.get("fullname"),
-            "creator_email": submission.get("email"),
-            "type_id": 1,
-            "priority_id": 1,
-            "subject": submission.get("subject"),
-            "contents": submission.get("content"),
-            "submit_general": "Submit"
-        },
-        headers={
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    )
-    print(f"Created form response code {r.status_code}")
-    text_response = r.text
-    # print(f"Created form response text : {text_response}")
-    response_message = re_obj.search(text_response)
-    if not response_message:
-        return "Could not create a ticket !"
-    print(response_message)
-    tree = html.fromstring(response_message.group())
-    ticket_id = tree.xpath("strong")[0].text_content()
-    print(f"Ticket ID : {ticket_id}")
+
+    thread = Thread(target=send_create_ticket_request, kwargs=submission)
+    thread.start()
     return ""
